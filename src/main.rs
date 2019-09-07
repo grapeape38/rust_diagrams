f8e4f331aa9be429f5b39970ec79b5ef94ccc230
unknown <eric_russo>
2019-09-07 17:28:20 -0400

    drag lines

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,15 @@ extern crate gl;
 
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use std::ffi::{CString};
 
 mod render_gl;
 mod primitives;
-use render_gl::*;
 use primitives::{*};
 
 fn main() {
     let sdl = sdl2::init().unwrap();
     let video_subsystem = sdl.video().unwrap();
-    const VIEWPORT: Point = Point{x:1100, y:700};
+    const VIEWPORT: Point = Point{x:1100., y:700.};
     let window = video_subsystem
         .window("Game", VIEWPORT.x as u32, VIEWPORT.y as u32)
         .opengl()
@@ -27,39 +25,17 @@ fn main() {
     gl_attr.set_context_version(4,5);
     let _gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
 
-
     unsafe {
-        gl::Viewport(0, 0, VIEWPORT.x, VIEWPORT.y);
+        gl::Viewport(0, 0, VIEWPORT.x as i32, VIEWPORT.y as i32);
         gl::ClearColor(0.3, 0.3, 0.5, 1.0);
     }
 
-
-    let vert_shader = Shader::from_vert_source(
-        &CString::new(include_str!("triangle.vert")).unwrap()
-    ).unwrap();
-
-    let frag_shader = Shader::from_frag_source(
-        &CString::new(include_str!("triangle.frag")).unwrap()
-    ).unwrap();
-
-    let mut shaders = vec![vert_shader, frag_shader];
-
-    let program = Program::from_shaders(shaders.as_ref()).unwrap();
-
-    let geom_shader = Shader::from_geom_source(
-        &CString::new(include_str!("line.geom")).unwrap()
-    ).unwrap();
-
-    shaders.insert(1, geom_shader);
-    let line_prog = Program::from_shaders(shaders.as_ref()).unwrap();
-
-    let shape_ctx = DrawCtx::new(program, VIEWPORT);
-    let line_ctx = DrawCtx::new(line_prog, VIEWPORT);
+   let programs = PrimPrograms::new();
+   let draw_ctx = DrawCtx::new(&programs, VIEWPORT);
 
     let mut event_pump = sdl.event_pump().unwrap();
     
     let mut shapes = DrawList::new();
-    let mut lines = DrawList::new();
 
     shapes.add(ShapeBuilder::new() 
         .tri(100)
@@ -96,22 +72,22 @@ fn main() {
         .offset(600,200).get()
     );
 
-    lines.add(LineBuilder::new()
-        .points(200,200,400,400)
+    shapes.add(LineBuilder::new()
+        .points(200.,200.,400.,400.)
         .color(0, 255, 255).line_width(6.).get()
     );
 
-    lines.add(LineBuilder::new()
-        .points(200,200,400,200)
+    shapes.add(LineBuilder::new()
+        .points(834.,338.,1000.,450.)
         .color(255, 255, 255).get()
     );
 
-    lines.add(LineBuilder::new()
-        .points(400,200,400,400)
+    shapes.add(LineBuilder::new()
+        .points(400.,200.,400.,400.)
         .color(255, 0, 0).get()
     );
 
-    let mut drag_state = DragState { drag_shape: None, last_pt: Point{x:0,y:0} };
+    let mut drag_state = DragState { drag_item: None, last_pt: Point{x:0.,y:0.} };
 
     'main: loop {
         for event in event_pump.poll_iter() {
@@ -119,7 +95,7 @@ fn main() {
                 ev @ Event::MouseMotion{..} | 
                 ev @ Event::MouseButtonDown{..} | 
                 ev @ Event::MouseButtonUp{..} => {
-                    drag_state.handle_mouse_event(&ev, &mut shapes, &VIEWPORT);
+                   drag_state.handle_mouse_event(&mut shapes, &ev, &VIEWPORT);
                 }
                 Event::Quit {..} | 
                 Event::KeyDown { keycode: Some(Keycode::Escape), ..} => break 'main,
@@ -129,8 +105,7 @@ fn main() {
                 gl::Clear(gl::COLOR_BUFFER_BIT);
             }
 
-            shapes.draw_all(&shape_ctx);
-            lines.draw_all(&line_ctx);
+            shapes.draw_all(&draw_ctx);
 
             window.gl_swap_window();
         }
@@ -138,31 +113,31 @@ fn main() {
 }
 
 struct DragState {
-    drag_shape: Option<u32>,
+    drag_item: Option<u32>,
     last_pt: Point
 }
 
 impl DragState {
-    fn handle_mouse_event(&mut self, ev: &Event, shapes: &mut DrawList, vp: &Point) {
+    fn handle_mouse_event(&mut self, shapes: &mut DrawList, ev: &Event, vp: &Point) {
         match *ev {
             Event::MouseButtonDown { mouse_btn, x, y, .. } => {
-                if self.drag_shape.is_none() && mouse_btn == sdl2::mouse::MouseButton::Left {
-                    self.last_pt = Point{x,y};
-                    self.drag_shape = shapes.intersect(&self.last_pt, vp);
+                if self.drag_item.is_none() && mouse_btn == sdl2::mouse::MouseButton::Left {
+                    self.last_pt = Point{x: x as f32,y: y as f32};
+                    self.drag_item = shapes.click_shape(&self.last_pt, vp); 
                 }
             } 
             Event::MouseButtonUp{mouse_btn, .. } => {
                 if mouse_btn == sdl2::mouse::MouseButton::Left {
-                    self.drag_shape = None
+                    self.drag_item = None
                 }
             }
             Event::MouseMotion{ x, y, ..} => {
-                if let Some(id) = self.drag_shape {
-                    let off = Point{x, y};
-                    if let Some(shape) = shapes.get_mut(&id) {
+                if let Some(id) = self.drag_item {
+                    if let Some(shape) = shapes.get_mut(id) {
+                        let off = Point{x: x as f32, y: y as f32};
                         shape.drag(off - self.last_pt);
+                        self.last_pt = off;
                     }
-                    self.last_pt = off;
                 }
             }
             _ => {}
--- src/primitives.rs
+++ src/primitives.rs
@@ -5,16 +5,58 @@ use nalgebra_glm as glm;
 use std::collections::HashMap;
 use std::ffi::CString;
 use std::f32::{self, consts::PI};
-use PrimType::*;
-use crate::render_gl::Program;
+use PrimType as PT;
+use crate::render_gl::{Shader, Program};
 
 type PrimMap = HashMap<PrimType, GLuint>;
 
 pub fn prim_map() -> PrimMap {
     let mut m = HashMap::new();
-    for prim in &[Triangle, Circle, Rect, Line] {
+    for prim in &[PT::Triangle, PT::Circle, PT::Rect, PT::Line] {
         m.insert(*prim, prim.buffer_data());
     }
+    m
+}
+
+type ProgMap<'a> = HashMap<PrimType, &'a Program>;
+
+pub struct PrimPrograms {
+    line_prog: Program,
+    shape_prog: Program
+}
+
+impl PrimPrograms {
+    pub fn new() -> PrimPrograms {
+        let vert_shader = Shader::from_vert_source(
+            &CString::new(include_str!("triangle.vert")).unwrap()
+        ).unwrap();
+
+        let frag_shader = Shader::from_frag_source(
+            &CString::new(include_str!("triangle.frag")).unwrap()
+        ).unwrap();
+
+        let mut shaders = vec![vert_shader, frag_shader];
+
+        let shape_prog = Program::from_shaders(shaders.as_ref()).unwrap();
+        let geom_shader = Shader::from_geom_source(
+            &CString::new(include_str!("line.geom")).unwrap()
+        ).unwrap();
+
+        shaders.insert(1, geom_shader);
+        let line_prog = Program::from_shaders(shaders.as_ref()).unwrap();
+        PrimPrograms {
+            shape_prog,
+            line_prog
+        }
+    }
+}
+
+pub fn prog_map(programs: &PrimPrograms) -> ProgMap {
+   let mut m = HashMap::new();
+   for prim in &[PrimType::Triangle, PrimType::Circle, PT::Rect] {
+        m.insert(*prim, &programs.shape_prog);
+    }
+    m.insert(PT::Line, &programs.line_prog);
     m
 }
 
@@ -31,14 +73,14 @@ const NCIRCLE_VERTS: usize = 30;
 impl PrimType {
     fn verts(&self) -> Vec<f32> {
         match self {
-            Triangle => { //isosceles
+            PT::Triangle => { //isosceles
                 vec![
                     -0.5, -0.5, 0.0,
                     0.5, -0.5, 0.0,
                     0.0, 0.5, 0.0
                 ]
             },
-            Circle => {
+            PT::Circle => {
                 let n = NCIRCLE_VERTS as f32;
                 let mut v = vec![0.0, 0.0, 0.0];
                 v.extend((0..NCIRCLE_VERTS).map(|i| 
@@ -46,7 +88,7 @@ impl PrimType {
                 ).flatten());
                 v
             },
-            Rect => {
+            PT::Rect => {
                vec![ 
                     -0.5, -0.5, 0.0,
                     -0.5, 0.5, 0.0,
@@ -56,7 +98,7 @@ impl PrimType {
                     0.5, -0.5, 0.0
                 ]
             },
-            Line => {
+            PT::Line => {
                vec![0.0, 0.0, 0.0]
             }
         }
@@ -66,31 +108,31 @@ impl PrimType {
     }
     fn mode(&self) -> GLenum {
         match self {
-            Triangle | Rect => gl::TRIANGLES,
-            Circle => gl::TRIANGLE_FAN,
-            Line => gl::LINES
+            PT::Triangle | PT::Rect => gl::TRIANGLES,
+            PT::Circle => gl::TRIANGLE_FAN,
+            PT::Line => gl::LINES
         }
     }
     fn size(&self) -> usize {
         match self {
-            Triangle => 3,
-            Rect => 6,
-            Circle => 3 * NCIRCLE_VERTS,
-            Line => 1
+            PT::Triangle => 3,
+            PT::Rect => 6,
+            PT::Circle => 3 * NCIRCLE_VERTS,
+            PT::Line => 1
         }
     }
-    fn in_bounds(&self, p: &(f32, f32)) -> bool {
+    fn in_bounds(&self, p: &Point) -> bool {
         match self {
-            Triangle => {
-                p.0 >= -0.5 && p.0 <= 0.5 && p.1 >= -0.5 && p.1 <= (0.5 - f32::abs(p.0))
+            PT::Triangle => {
+                p.x >= -0.5 && p.x <= 0.5 && p.y >= -0.5 && p.y <= (0.5 - f32::abs(p.x))
             }
-            Circle => {
-                (p.0 * p.0 + p.1 * p.1) <= 1.
+            PT::Circle => {
+                p.mag() <= 1.
             }
-            Rect => {
-                p.0 >= -0.5 && p.0 <= 0.5 && p.1 >= -0.5 && p.1 <= 0.5 
+            PT::Rect => {
+                p.x >= -0.5 && p.x <= 0.5 && p.y >= -0.5 && p.y <= 0.5 
             }
-            Line => {
+            PT::Line => {
                 false
             }
         }
@@ -131,22 +173,26 @@ pub enum TypeParams {
 impl TypeParams {
     fn ptype(&self) -> PrimType {
         match self {
-            TypeParams::Triangle {..} => Triangle,
-            TypeParams::Rect {..} => Rect,
-            TypeParams::Ellipse {..} => Circle
+            TypeParams::Triangle {..} => PT::Triangle,
+            TypeParams::Rect {..} => PT::Rect,
+            TypeParams::Ellipse {..} => PT::Circle
         }
     }
 }
 
 #[derive(Debug, Clone, Copy)]
 pub struct Point {
-    pub x: i32,
-    pub y: i32
+    pub x: f32,
+    pub y: f32
 }
 
 impl Point {
     fn mag(&self) -> f32 {
-        f32::sqrt((self.x * self.x + self.y * self.y) as f32)
+        f32::sqrt((self.x*self.x + self.y*self.y) as f32)
+    }
+    fn dist(&self, p2: &Point) -> f32 {
+        let d = *self - *p2;
+        d.mag()
     }
 }
 
@@ -178,18 +224,56 @@ impl std::ops::Sub for Point {
     }
 }
 
+#[derive(Debug)]
 pub struct Line {
+    a: f32, b: f32, c: f32 
+}
+
+fn det(a: f32, b: f32, c: f32, d: f32) -> f32 {
+    a * d - b * c
+}
+
+impl Line {
+    fn from_pts(p1: Point, p2: Point) -> Line {
+        let a = p1.y - p2.y;
+        let b = p2.x - p1.x;
+        Line {
+            a, b, c: -(a * p1.x + b * p1.y)
+        }
+    }
+    fn from_pt_slope(p: &Point, l: Line) -> Line {
+        Line { c: -(l.a * p.x + l.b * p.y), ..l }
+    }
+    fn intersect(&self, l2: &Line) -> Option<Point> {
+        let zn = det(self.a, self.b, l2.a, l2.b);
+        if f32::abs(zn) < 1e-7 {
+            return None;
+        }
+        Some(Point {
+            x: -det(self.c, self.b, l2.c, l2.b) / zn,
+            y: -det(self.a, self.c, l2.a, l2.c) / zn
+        })
+    }
+    fn dist_to_pt(&self, p: &Point) -> f32 {
+        let opp = Line { a: -self.b, b: self.a, c: 0.0 };
+        let l2 = Line::from_pt_slope(p, opp);
+        let inter = self.intersect(&l2).unwrap();
+        p.dist(&inter)
+    }
+}
+
+pub struct DrawLine {
     p1: Point,
     p2: Point,
     color: (u8, u8, u8),
     line_width: f32
 }
 
-impl Line {
-    pub fn new() -> Line {
-        Line {
-            p1: Point {x: 0, y: 0},
-            p2: Point {x: 0, y: 0},
+impl DrawLine {
+    pub fn new() -> DrawLine {
+        DrawLine {
+            p1: Point {x: 0., y: 0.},
+            p2: Point {x: 0., y: 0.},
             color: (0, 0, 0),
             line_width: 3.
         }
@@ -197,57 +281,32 @@ impl Line {
 }
 
 pub struct LineBuilder {
-    l: Line
+    l: DrawLine
 }
 
 impl LineBuilder {
     pub fn new() -> LineBuilder {
-        LineBuilder { l: Line::new() }
+        LineBuilder { l: DrawLine::new() }
     }
-    pub fn points(mut self, x1: i32, y1: i32, x2: i32, y2: i32) -> LineBuilder {
+    pub fn points(mut self, x1: f32, y1: f32, x2: f32, y2: f32) -> Self {
         self.l.p1 = Point {x: x1, y: y1};
         self.l.p2 = Point {x: x2, y: y2};
         self
     }
-    pub fn color(mut self, r: u8, g: u8, b: u8) -> LineBuilder {
+    pub fn color(mut self, r: u8, g: u8, b: u8) -> Self {
         self.l.color = (r,g,b);
         self
     }
-    pub fn line_width(mut self, width: f32) -> LineBuilder {
+    pub fn line_width(mut self, width: f32) -> Self {
         self.l.line_width = width;
         self
     }
-    pub fn get(self) -> Line { self.l }
+    pub fn get(self) -> DrawLine { self.l }
 }
 
-impl From<LineBuilder> for Line {
+impl From<LineBuilder> for DrawLine {
     fn from(lb: LineBuilder) -> Self {
         lb.l
-    }
-}
-
-pub struct DrawList<'a> {
-    m: HashMap<u32, Box<Drawable + 'a>>,
-    next_id: u32
-}
-
-impl<'a> DrawList<'a> {
-    pub fn new() -> DrawList<'a> {
-        DrawList {m: HashMap::new(), next_id: 0}
-    }
-    pub fn add<D: Drawable + 'a>(&mut self, s: D) {
-        self.m.insert(self.next_id, Box::new(s));
-        self.next_id+=1;
-    }
-    pub fn get_mut(&mut self, id: &u32) -> Option<&mut Box<Drawable + 'a>> {
-        self.m.get_mut(id)
-    }
-    pub fn intersect(&self, p: &Point, vp: &Point) -> Option<u32> {
-        self.m.iter().find(|(_,v)| v.in_bounds(p, vp)).map(|(k,_)| *k)
-    }
-    pub fn draw_all(&self, ctx: &DrawCtx) {
-        ctx.program.set_used();
-        self.m.values().for_each(|s| s.draw(ctx));
     }
 }
 
@@ -268,57 +327,51 @@ impl ShapeBuilder {
     pub fn new() -> ShapeBuilder {
         ShapeBuilder { s: Shape::new() }
     }
-    pub fn offset(mut self, x: i32, y: i32) -> ShapeBuilder {
-        self.s.offset = Point {x,y};
+    pub fn offset(mut self, x: i32, y: i32) -> Self {
+        self.s.offset = Point {x: x as f32,y: y as f32};
         self
     }
     pub fn rot(mut self, rot: f32) -> ShapeBuilder {
         self.s.rot = rot;
         self
     }
-    pub fn color(mut self, r: u8, g: u8, b: u8) -> ShapeBuilder {
+    pub fn color(mut self, r: u8, g: u8, b: u8) -> Self {
         self.s.color = (r,g,b);
         self
     }
-
-    pub fn line_width(mut self, width: f32) -> ShapeBuilder {
+    pub fn line_width(mut self, width: f32) -> Self {
         self.s.line_width = width;
         self
     }
-
-    pub fn circle(mut self, rad: u32) -> ShapeBuilder {
+    pub fn circle(mut self, rad: u32) -> Self {
         self.s.params = TypeParams::Ellipse{
             rad_x: rad,
             rad_y: rad
         };
         self
     }
-
-    pub fn ellipse(mut self, rad_x: u32, rad_y: u32) -> ShapeBuilder {
+    pub fn ellipse(mut self, rad_x: u32, rad_y: u32) -> Self {
         self.s.params = TypeParams::Ellipse{
             rad_x,
             rad_y
         };
         self
     }
-
-    pub fn rect(mut self, width: u32, height: u32) -> ShapeBuilder {
+    pub fn rect(mut self, width: u32, height: u32) -> Self {
         self.s.params = TypeParams::Rect {
             width,
             height
         };
         self
     }
-
-    pub fn square(mut self, side: u32) -> ShapeBuilder {
+    pub fn square(mut self, side: u32) -> Self {
         self.s.params = TypeParams::Rect {
             width: side,
             height: side
         };
         self
     }
-
-    pub fn tri(mut self, base: u32) -> ShapeBuilder {
+    pub fn tri(mut self, base: u32) -> Self {
         self.s.params = TypeParams::Triangle {
             base
         };
@@ -337,7 +390,7 @@ impl Shape {
     pub fn new() -> Shape {
         Shape {
             params: TypeParams::Triangle{base: 5},
-            offset: Point{x:0,y:0},
+            offset: Point{x:0.,y:0.},
             rot: 0.,
             line_width: 3.,
             color: (0, 0, 0)
@@ -371,79 +424,142 @@ impl Shape {
     fn size(&self) -> GLint { self.params.ptype().size() as GLint }
 }
 
-pub trait Drawable {
-    fn draw(&self, ctx: &DrawCtx);
-    fn drag(&mut self, dir: Point);
+pub trait InBounds {
     fn in_bounds(&self, p: &Point, vp: &Point) -> bool;
 }
+
+pub trait Draggable {
+    fn drag(&mut self, to: Point);
+}
+
+pub trait Drawable {
+    fn draw(&self, ctx: &DrawCtx);
+    fn prim_type(&self) -> PrimType;
+}
+
+pub trait DrawBounds: InBounds + Drawable + Draggable { }
 
 impl Drawable for Shape {
     fn draw(&self, ctx: &DrawCtx) {
         let trans = self.trans(&ctx.viewport);
         let color = rgb_to_f32(&self.color);
+        let prog_id = ctx.prog_map[&self.prim_type()].id();
+        let vao = ctx.prim_map[&self.prim_type()];
         unsafe {
-            gl::BindVertexArray(ctx.prim_map[&self.params.ptype()]);
-            let trans_loc = gl::GetUniformLocation(ctx.program.id(), GChar::new("transform").ptr());
+            let trans_loc = gl::GetUniformLocation(prog_id, GChar::new("transform").ptr());
             gl::UniformMatrix4fv(trans_loc, 1, gl::FALSE, trans.as_ptr());
-            let color_loc = gl::GetUniformLocation(ctx.program.id(), GChar::new("color").ptr());
+            let color_loc = gl::GetUniformLocation(prog_id, GChar::new("color").ptr());
             gl::Uniform4f(color_loc, color.0, color.1, color.2, 1.0);
             gl::LineWidth(self.line_width as GLfloat);
+            gl::BindVertexArray(vao);
             gl::DrawArrays(self.mode(), 0, self.size());
         }
     }
-    fn drag(&mut self, dir: Point) {
-        self.offset += dir;
-    }
-    fn in_bounds(&self, p: &Point, vp: &Point) -> bool {
-        let trans_inv = glm::inverse(&self.trans(vp));
-        let pc = pixels_to_trans_vec(p, vp);
-        let normpt = trans_inv * glm::vec4(pc.x, pc.y, pc.z, 1.0);
-        self.params.ptype().in_bounds(&(normpt.x, normpt.y))
+    fn prim_type(&self) -> PrimType {
+        self.params.ptype()
     }
 }
 
-impl Drawable for Line {
+impl InBounds for Shape {
+     fn in_bounds(&self, p: &Point, vp: &Point) -> bool {
+        let trans_inv = glm::inverse(&self.trans(vp));
+        let pc = pixels_to_trans_vec(p, vp);
+        let normpt = trans_inv * glm::vec4(pc.x, pc.y, pc.z, 1.0);
+        self.params.ptype().in_bounds(&Point {x: normpt.x, y: normpt.y})
+    }
+}
+
+impl Draggable for Shape {
+    fn drag(&mut self, off: Point) {
+        self.offset += off;
+    }
+}
+
+impl Drawable for DrawLine {
     fn draw(&self, ctx: &DrawCtx) {
         let mut trans: glm::TMat4<f32> = glm::identity();
         let p1c = pixels_to_trans_vec(&self.p1, &ctx.viewport);
         let p2c = pixels_to_trans_vec(&self.p2, &ctx.viewport);
         trans = glm::translate(&trans, &p1c);
         let color = rgb_to_f32(&self.color);
+        let prog_id = ctx.prog_map[&self.prim_type()].id();
+        let vao = ctx.prim_map[&self.prim_type()];
         unsafe {
-            gl::BindVertexArray(ctx.prim_map[&PrimType::Line]);
-            let trans_loc = gl::GetUniformLocation(ctx.program.id(), GChar::new("transform").ptr());
+            let trans_loc = gl::GetUniformLocation(prog_id, GChar::new("transform").ptr());
             gl::UniformMatrix4fv(trans_loc, 1, gl::FALSE, trans.as_ptr());
-            let color_loc = gl::GetUniformLocation(ctx.program.id(), GChar::new("color").ptr());
+            let color_loc = gl::GetUniformLocation(prog_id, GChar::new("color").ptr());
             gl::Uniform4f(color_loc, color.0, color.1, color.2, 1.0);
-            let point2_loc = gl::GetUniformLocation(ctx.program.id(), GChar::new("point2").ptr());
+            let point2_loc = gl::GetUniformLocation(prog_id, GChar::new("point2").ptr());
             gl::Uniform2f(point2_loc, p2c.x, p2c.y);
             gl::LineWidth(self.line_width as GLfloat);
+            gl::BindVertexArray(vao);
             gl::DrawArrays(gl::POINTS, 0, 1);
         }
     }
-    fn drag(&mut self, dir: Point) {
-        self.p1 += dir;
+    fn prim_type(&self) -> PrimType {
+        PT::Line
     }
+}
+
+impl InBounds for DrawLine {
     fn in_bounds(&self, p: &Point, _: &Point) -> bool {
-        let t = (p.x - self.p1.x) as f32 / (self.p2.x - self.p1.x) as f32;
-        let ypt = (1. - t) * self.p1.y as f32 + t * self.p2.y as f32;
-        t >= 0. && t <= 1. && p.y as f32 - ypt <= self.line_width
+        let mn = if self.p1.x < self.p2.x { self.p1.x } else { self.p2.x };
+        let mx = if self.p1.x < self.p2.x { self.p2.x } else { self.p1.x };
+        if p.x < mn || p.x > mx {
+            return false;
+        }
+        let l = Line::from_pts(self.p1, self.p2);
+        let dst = l.dist_to_pt(p);
+        dst <= self.line_width
     }
 }
 
-pub struct DrawCtx {
+impl Draggable for DrawLine {
+    fn drag(&mut self, off: Point) {
+        self.p1 += off;
+        self.p2 += off;
+    }
+}
+
+impl DrawBounds for Shape {}
+impl DrawBounds for DrawLine {}
+
+pub struct DrawCtx<'a> {
     prim_map: PrimMap,
+    prog_map: ProgMap<'a>,
     viewport: Point,
-    program: Program 
 }
 
-impl DrawCtx {
-    pub fn new(program: Program, viewport: Point) -> DrawCtx {
-        DrawCtx { prim_map: prim_map(), viewport, program }
+impl<'a> DrawCtx<'a> {
+    pub fn new(programs: &'a PrimPrograms, viewport: Point) -> DrawCtx<'a> {
+        DrawCtx { prim_map: prim_map(), prog_map: prog_map(programs), viewport }
     }
-    pub fn draw<D: Drawable>(&self, s: &D) {
-        self.program.set_used();
-        s.draw(&self);
+}
+
+pub struct DrawList<'a> {
+    m: HashMap<u32, Box<DrawBounds + 'a>>,
+    next_id: u32
+}
+
+impl<'a> DrawList<'a> {
+    pub fn new() -> DrawList<'a> {
+        DrawList {m: HashMap::new(), next_id: 0}
+    }
+    pub fn add<D: DrawBounds + 'a>(&mut self, s: D) {
+        self.m.insert(self.next_id, Box::new(s));
+        self.next_id += 1;
+    }
+    pub fn get_mut(&mut self, id: u32) -> Option<&mut Box<DrawBounds +'a>> {
+        self.m.get_mut(&id)
+    }
+    pub fn click_shape(&'a self, p: &Point, vp: &Point) -> Option<u32> {
+        self.m.iter().find(|(_,s)| s.in_bounds(p, vp)).map(|(k,_)| *k)
+    }
+    pub fn draw_all(&self, ctx: &DrawCtx) {
+        self.m.iter().for_each(|(_, s)| {
+            ctx.prog_map[&s.prim_type()].set_used();
+            s.draw(ctx);
+        });
     }
 }
 
@@ -453,11 +569,11 @@ fn rgb_to_f32(rgb: &(u8, u8, u8)) -> (f32, f32, f32) {
 
 fn pixels_to_trans_vec(pixels: &Point, vp: &Point) -> glm::TVec3<f32> {
     let coords = pixels_to_coords(pixels, vp);
-    glm::vec3(coords.0, coords.1, 0.0)
+    glm::vec3(coords.x, coords.y, 0.0)
 }
 
-fn pixels_to_coords(pixels: &Point, vp: &Point) -> (f32, f32) {
-    (-1. + 2. * pixels.x as f32 / vp.x as f32, 1. - 2. * pixels.y as f32 / vp.y as f32)
+fn pixels_to_coords(pixels: &Point, vp: &Point) -> Point {
+    Point {x: -1. + 2. * pixels.x  / vp.x , y: 1. - 2. * pixels.y  / vp.y}
 }
 
 #[allow(dead_code)]
